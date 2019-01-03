e834f15e896739a9e4899461a95132cf8ff1cd88
Your Name <you@example.com>
2019-01-03 15:44:29 +0800

    develop

--- Assets/3.scripts/TankMove.cs
+++ Assets/3.scripts/TankMove.cs
@@ -5,9 +5,9 @@ public class TankMove : MonoBehaviour {
 
 	public float mSpeed = 1;//移動速度
 	public float rSpeed = 1;//旋轉速度
-
-	// Use this for initialization
-	void Start () {
+    public float lSpeed = 1;
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
